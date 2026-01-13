8312776568f648473532464e79c0e465f610d019
kangsuyeong <ekdh16@naver.com>
2026-01-13 22:32:07 +0900

    5-13

--- hooks/queries/useLikePost.ts
+++ hooks/queries/useLikePost.ts
@@ -1,17 +1,58 @@
 import { likePost } from '@/api/post';
 import queryClient from '@/api/queryClient';
 import { queryKey } from '@/constants';
+import { Post, Profile } from '@/types';
 import { useMutation } from '@tanstack/react-query';
 
 function useLikePost() {
   return useMutation({
     mutationFn: likePost,
-    onSuccess: (postId) => {
+    onMutate: async (postId) => {
+      await queryClient.cancelQueries({
+        queryKey: [queryKey.POST, queryKey.GET_POST, postId],
+      });
+
+      const user = queryClient.getQueryData<Profile>([
+        queryKey.AUTH,
+        queryKey.GET_ME,
+      ]);
+      const userId = Number(user?.id);
+      const previousPost = queryClient.getQueryData<Post>([
+        queryKey.POST,
+        queryKey.GET_POST,
+        postId,
+      ]);
+
+      const newPost = { ...previousPost };
+      const likedIndex =
+        previousPost?.likes.findIndex((like) => like.userId === userId) ?? -1;
+
+      if (likedIndex >= 0) {
+        newPost.likes?.splice(likedIndex, 1);
+      } else {
+        newPost.likes?.push({ userId });
+      }
+
+      queryClient.setQueryData(
+        [queryKey.POST, queryKey.GET_POST, postId],
+        newPost
+      );
+
+      return { previousPost, newPost };
+    },
+    onError: (err, newPost, context) => {
+      queryClient.setQueryData(
+        [queryKey.POST, queryKey.GET_POST, context?.previousPost?.id],
+        context?.previousPost
+      );
+    },
+    onSettled: (data, error, variables, context) => {
+      console.log('variables', variables);
       queryClient.invalidateQueries({
-        queryKey: [queryKey.POST, queryKey.GET_POSTS],
+        queryKey: [queryKey.POST, queryKey.GET_POST, variables],
       });
       queryClient.invalidateQueries({
-        queryKey: [queryKey.POST, queryKey.GET_POST, postId],
+        queryKey: [queryKey.POST, queryKey.GET_POSTS],
       });
     },
   });
