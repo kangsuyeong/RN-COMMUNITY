d70e57dce0b86f5e9c76afb6da5fa4092688e9c8
kangsuyeong <ekdh16@naver.com>
2026-01-23 15:28:46 +0900

    6-6

--- app/profile/avatar.tsx
+++ app/profile/avatar.tsx
@@ -1,3 +1,4 @@
+import { baseUrls } from '@/api/axios';
 import AvatarItem from '@/components/AvatarItem';
 import FixedBottomCTA from '@/components/FixedBottomCTA';
 import Tab from '@/components/Tab';
@@ -6,8 +7,9 @@ import useAuth from '@/hooks/queries/useAuth';
 import useGetAvatarItems from '@/hooks/queries/useGetAvatarItems';
 import { useNavigation } from 'expo-router';
 import { useEffect, useRef, useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, Platform, StyleSheet, View } from 'react-native';
 import PagerView from 'react-native-pager-view';
+import { SvgUri } from 'react-native-svg';
 import Toast from 'react-native-toast-message';
 
 export default function AvatarScreen() {
@@ -22,7 +24,7 @@ export default function AvatarScreen() {
     topId: auth?.topId ?? '',
     bottomId: auth?.bottomId ?? '',
     handId: auth?.handId ?? '',
-    skinId: auth?.skinId ?? '',
+    skinId: auth?.skinId ?? '01',
   });
 
   console.log('avatarItem', avatarItem);
@@ -40,6 +42,16 @@ export default function AvatarScreen() {
   const handlePressTab = (index: number) => {
     pagerRef.current?.setPage(index);
     SetCurrentTab(index);
+  };
+
+  const getAvatarItemUrl = (category: string, id?: string) => {
+    const baseUrl = Platform.OS === 'ios' ? baseUrls.ios : baseUrls.android;
+
+    if (category === 'default' || !Boolean(id)) {
+      return `${baseUrl}/default/frame.svg`;
+    }
+
+    return `${baseUrl}/items/${category}/${id}.svg`;
   };
 
   const handleSaveAvatar = () => {
@@ -63,6 +75,50 @@ export default function AvatarScreen() {
   return (
     <>
       <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View style={styles.avatarContainer}>
+            {avatarItem.hatId && (
+              <SvgUri
+                uri={getAvatarItemUrl('hats', avatarItem.hatId)}
+                style={[styles.avatar, { zIndex: 70 }]}
+              />
+            )}
+            {avatarItem.faceId && (
+              <SvgUri
+                uri={getAvatarItemUrl('faces', avatarItem.faceId)}
+                style={[styles.avatar, { zIndex: 60 }]}
+              />
+            )}
+            {avatarItem.topId && (
+              <SvgUri
+                uri={getAvatarItemUrl('tops', avatarItem.topId)}
+                style={[styles.avatar, { zIndex: 50 }]}
+              />
+            )}
+            {avatarItem.bottomId && (
+              <SvgUri
+                uri={getAvatarItemUrl('bottoms', avatarItem.bottomId)}
+                style={[styles.avatar, { zIndex: 40 }]}
+              />
+            )}
+            <SvgUri
+              uri={getAvatarItemUrl('default')}
+              style={[styles.avatar, { zIndex: 30 }]}
+            />
+            {avatarItem.skinId && (
+              <SvgUri
+                uri={getAvatarItemUrl('skins', avatarItem.skinId)}
+                style={[styles.avatar, { zIndex: 20 }]}
+              />
+            )}
+            {avatarItem.handId && (
+              <SvgUri
+                uri={getAvatarItemUrl('hands', avatarItem.handId)}
+                style={[styles.avatar, { zIndex: 10 }]}
+              />
+            )}
+          </View>
+        </View>
         <View style={styles.tabContainer}>
           {['모자', '얼굴', '상의', '하의', '손', '피부'].map((tab, index) => (
             <Tab
@@ -89,6 +145,7 @@ export default function AvatarScreen() {
             { data: skins, name: 'skinId', id: avatarItem.skinId },
           ].map((list) => (
             <FlatList
+              key={list.name}
               data={list.data}
               keyExtractor={(item, index) => String(index)}
               numColumns={3}
@@ -123,5 +180,27 @@ const styles = StyleSheet.create({
     paddingBottom: 120,
     marginTop: 10,
     alignItems: 'center',
+  },
+  headerContainer: {
+    alignItems: 'center',
+    position: 'relative',
+    backgroundColor: colors.ORANGE_200,
+    width: '100%',
+    height: 115,
+    marginBottom: 115,
+  },
+  avatarContainer: {
+    width: 229,
+    height: 229,
+    borderRadius: 229,
+    overflow: 'hidden',
+    borderWidth: 1,
+    borderColor: colors.GRAY_200,
+    backgroundColor: colors.WHITE,
+  },
+  avatar: {
+    width: 229,
+    height: 229,
+    position: 'absolute',
   },
 });
